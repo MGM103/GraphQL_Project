8407ed31a8f03c8c6b346e1a9a994e43ba6b37aa
MGM103 <marcus.marinelli3@gmail.com>
2023-02-07 11:30:58 +1100

    Fixing URI visibility

--- ReactGQL/server/server.js
+++ ReactGQL/server/server.js
@@ -4,11 +4,12 @@ const expressGraphQL = require('express-graphql');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const schema = require('./schema/schema');
+require('dotenv').config();
 
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = 'mongodb+srv://admin:Marcus123@cluster0.v7lkdyi.mongodb.net/?retryWrites=true&w=majority';
+const MONGO_URI = process.env.MONGODB_URI;
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
